7948b31ebb41bebc95acd3f4d5f4f8c65524862e
jpd800 <jacob.duffy@fammed.wisc.edu>
2016-04-19 15:08:09 -0500

    Made the Nav's menu items look nice.

--- app/helpers/dfm_web_helper.rb
+++ app/helpers/dfm_web_helper.rb
@@ -1,8 +1,5 @@
 module DfmWebHelper
 
-  def menu_tag( title, link, options = {})
-    link_to(content_tag(:li, title, options), link)
-  end
 
   def excel_icon(link = params.merge(:format => "xlsx"))
     # Create Excel icon in consistent location
