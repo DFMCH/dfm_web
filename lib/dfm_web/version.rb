cf5d7247e853adc404d62a4aa76f5993604106e7
jpd800 <jacob.duffy@fammed.wisc.edu>
2016-08-09 08:46:34 -0500

    Version bump to 1.0.0 and listing of Version History in version.rb

--- lib/dfm_web/version.rb
+++ lib/dfm_web/version.rb
@@ -1,3 +1,13 @@
 module DfmWeb
-  VERSION = "0.0.6"
+  VERSION = "1.0.0"
 end
+
+
+# Version History
+
+# 1.0.0   Version bump.  We're in production on three apps already.
+# 0.0.5   Added .warn and .alert modifier classes for most typography and .panel
+# 0.0.4   Added :active class for tables to show a checkmark used in a few apps.
+# 0.0.3   Lowered coffee-rails version Requirement
+# 0.0.2   Lowered Rails version Requirement
+# 0.0.1   Initial Commit
