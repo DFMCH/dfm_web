860ad8cde3aaf837be3b841aec39286c196a5254
duffyjp <duffy.jp@gmail.com>
2018-02-16 13:47:31 -0600

    Added dfm_web assets to the pipeline so you don't have to.

--- lib/dfm_web/engine.rb
+++ lib/dfm_web/engine.rb
@@ -1,4 +1,5 @@
 module DfmWeb
   class Engine < ::Rails::Engine
+    Engine.config.assets.precompile += %w( dfm_web/* )
   end
 end
--- lib/dfm_web/version.rb
+++ lib/dfm_web/version.rb
@@ -1,10 +1,11 @@
 module DfmWeb
-  VERSION = "1.1.0"
+  VERSION = "1.1.1"
 end
 
 
 # Version History
 
+# 1.1.1   Added dfm_web assets to the pipeline so you don't have to.
 # 1.1.0   In order to support both Turbolinks and non-Turbolinks sites, DfmWeb must be manually activated now.
 # 1.0.7   Copied simple versions of _nav and _footer from the dummy app so they won't be missing assets on new apps.
 # 1.0.6   Added "paper" class to simulate printed content.
