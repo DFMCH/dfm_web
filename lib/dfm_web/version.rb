fdb238feb0d8ea26dee5e9d0a01666be8f1adf58
duffyjp <duffy.jp@gmail.com>
2018-04-10 13:02:32 -0500

    Eliminate a visual jump when resizing from L to XL.

--- lib/dfm_web/version.rb
+++ lib/dfm_web/version.rb
@@ -1,10 +1,11 @@
 module DfmWeb
-  VERSION = "2.1.0"
+  VERSION = "2.1.1"
 end
 
 
 # Version History
 
+# 2.1.1   Eliminate a visual jump when resizing from L to XL.
 # 2.1.0   Updated the print version of the "paper" class so it matches the screen version as it should.
 # 2.0.9   Added a animation clear on the notice/alert close javascript. This allows you to add new actions in your app.
 # 2.0.8   Refreshed Print styles.  Hides Nav, Footer and removes empty space around major elements.
