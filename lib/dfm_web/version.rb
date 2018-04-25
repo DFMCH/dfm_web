6f5e2a0fd3856710dd33863debfb913c8bca5fd4
duffyjp <duffy.jp@gmail.com>
2018-04-25 08:45:53 -0500

    Bump version & changelog for release.

--- lib/dfm_web/version.rb
+++ lib/dfm_web/version.rb
@@ -1,10 +1,11 @@
 module DfmWeb
-  VERSION = "2.1.4"
+  VERSION = "2.1.5"
 end
 
 
 # Version History
 
+# 2.1.5   Added explicit transparent background on body to aid combining DfmWeb with other CSS frameworks.
 # 2.1.4   Fixed menu bug when browser was exactly 1037px wide with a scrollbar.
 # 2.1.3   New Table TR modifier classes: okay, highlight, warn, alert, gray.
 # 2.1.2   Add submit buttons w/ modifier classes below "button" links with the same classes to the kitchen sink.
