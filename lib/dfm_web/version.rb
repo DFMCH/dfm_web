3c0007ac7582972f8c0e3721172dc2fa21582f82
Jacob Duffy <duffy.jp@gmail.com>
2019-01-14 10:35:26 -0600

    Force right-side NAV dropdown menus to use space to the left.

--- lib/dfm_web/version.rb
+++ lib/dfm_web/version.rb
@@ -1,10 +1,11 @@
 module DfmWeb
-  VERSION = "2.2.1"
+  VERSION = "2.2.2"
 end
 
 
 # Version History
 
+# 2.2.2   Force right-side NAV dropdown menus to use space to the left.  Prevents menus from causing document level horizontal scroll bars.
 # 2.2.1   Fix Chrome table printing bug.  Add "table_hover" class for cells you only want to display on hover.
 # 2.2.0   Update CSS comments dealing with footer options.
 # 2.1.9   Built in support for always visible footer.
