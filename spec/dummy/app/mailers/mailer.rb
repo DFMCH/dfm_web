69e85756df5d006542a44e91903618d675e0bf74
Jacob Duffy <duffy.jp@gmail.com>
2020-03-17 14:10:54 -0500

    Added a new mailer template that emulates the app style with really basic CSS.

--- spec/dummy/app/mailers/mailer.rb
+++ spec/dummy/app/mailers/mailer.rb
@@ -2,11 +2,12 @@ class Mailer < ActionMailer::Base
 
   default from: 'DFM Web <dfm_web@example.com>'
   default to:   'Example Person <example@example.com>'
+  default content_type: 'text/html'
   layout        'mailer'
 
 
   def example(options = {})
-    mail(default_params.merge(options))
+    mail(options)
   end
 
 end
