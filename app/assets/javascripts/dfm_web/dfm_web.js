07c71c6c7b76b7e5dba7b611dd06407f257423bf
Jacob Duffy <duffy.jp@gmail.com>
2022-03-02 11:32:21 -0600

    Small refactoring and add (optional) polyfill to support Internet Explorer 11.

--- app/assets/javascripts/dfm_web/dfm_web.js
+++ app/assets/javascripts/dfm_web/dfm_web.js
@@ -22,13 +22,13 @@ window.DfmWeb = {};
 
 DfmWeb.activate_dfm_web = function() {
   // Hide the #notice and #alert messages by clicking the [X] or pressing escape key
-  document.querySelectorAll('#notice, #alert').forEach((node) => {
-    node.addEventListener('click', () => {
+  document.querySelectorAll('#notice, #alert').forEach(function (node) {
+    node.addEventListener('click', function () {
       node.style.display = 'none';
     });
 
     // If either ID exists, close by pressing Escape
-    document.addEventListener('keyup', (key) => {
+    document.addEventListener('keyup', function (key) {
       if (key.code == 'Escape') {
         node.style.display = 'none';
       }
@@ -40,16 +40,16 @@ DfmWeb.activate_dfm_web = function() {
   // Insert the Hamburger if there are menu 2+ items
   // Add "has_hamburger" class to the ul so CSS can know which way to show it.
   if (document.querySelectorAll('#nav ul.right>li').length > 1) {
-    const hamburger = document.createElement('div');
+    var hamburger = document.createElement('div');
     hamburger.setAttribute('id', 'hamburger');
     document.querySelector('#nav ul.right').after(hamburger);
     document.querySelector('ul.right').classList.add('has_hamburger');
   }
 
   // Show the Mobile Menu on Hamburger Click
-  document.querySelectorAll('nav #hamburger').forEach((node) => {
-    node.addEventListener('click', () => {
-      document.querySelectorAll('nav #nav ul.has_hamburger').forEach((node) => {
+  document.querySelectorAll('nav #hamburger').forEach(function (node) {
+    node.addEventListener('click', function () {
+      document.querySelectorAll('nav #nav ul.has_hamburger').forEach(function (node) {
         node.style.display = node.style.display === 'inline-block' ? 'none' : 'inline-block';
       });
     });
@@ -59,21 +59,21 @@ DfmWeb.activate_dfm_web = function() {
   // If you've toggled the Mobile menu it breaks larger sizes.  Reset on resize.
   window.onresize = function() {
     if (window.innerWidth >= 1024) {
-      document.querySelectorAll('nav #nav ul.has_hamburger').forEach((node) => {
+      document.querySelectorAll('nav #nav ul.has_hamburger').forEach(function (node) {
         node.style.display = 'inline-block';
       });
     } else {
-      document.querySelectorAll('nav #nav ul.has_hamburger').forEach((node) => {
+      document.querySelectorAll('nav #nav ul.has_hamburger').forEach(function (node) {
         node.style.display = 'none';
       });
     }
   };
 
   // iPads don't have :hover, so hide the menu if the user clicks anything in <main>
-  document.querySelectorAll('main').forEach((node) => {
-    node.addEventListener('click', () => {
+  document.querySelectorAll('main').forEach(function (node) {
+    node.addEventListener('click', function () {
       if (window.innerWidth < 1024) {
-        document.querySelectorAll('nav #nav ul.has_hamburger').forEach((node) => {
+        document.querySelectorAll('nav #nav ul.has_hamburger').forEach(function (node) {
           node.style.display = 'none';
         });
       }
@@ -82,7 +82,7 @@ DfmWeb.activate_dfm_web = function() {
 
   // Deal with Really long menus
   // Add "crowded" class to make them more compact.
-  document.querySelectorAll('#nav > ul > li > ul').forEach((node) => {
+  document.querySelectorAll('#nav > ul > li > ul').forEach(function (node) {
     if (node.children.length > 10) {
       node.classList.add('crowded');
     }
